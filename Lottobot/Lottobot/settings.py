6533ac8a63692e157a743e9a43612867f4b67926
jongyeol2 <114922306+jongyeol2@users.noreply.github.com>
2025-02-13 10:51:57 +0900

    modify: settings.py

--- Lottobot/Lottobot/settings.py
+++ Lottobot/Lottobot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
     
     # Local apps
     'accounts',
+    'chatbot',
     
 ]
 
